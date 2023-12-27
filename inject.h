81224ae8e3d3b06d1bcc32a1d08be7f7998bd7b9
nyiyui <+@nyiyui.ca>
2023-12-27 01:30:28 -0500

    Revert "Revert "fail""
    This reverts commit 8f21ce2ce4e287f0582dc29604e77955e5df0fbb.

--- inject.h
+++ inject.h
@@ -1,3 +1,8 @@
+float coeff_proportion = 0.1;
+float coeff_integral = 0;
+float coeff_derivative = 0.3;
+float deltaBase = 1.3;
+
 void inject() {
   if (Serial.available()) {
     int ctl = Serial.read();
@@ -18,6 +23,34 @@ void inject() {
           int ctl = Serial.read();
           if (ctl == 'p') {
             break;
+          } else if (ctl == 'c') {
+            motor_write(0, 0);
+            motor_write(1, 0);
+            light_calibration_mode();
+          } else if (ctl == 'R') {
+            coeff_proportion += 0.1;
+            Serial.print("coeff_proportion = ");
+            Serial.println(coeff_proportion);
+          } else if (ctl == 'r') {
+            coeff_proportion -= 0.1;
+            Serial.print("coeff_proportion = ");
+            Serial.println(coeff_proportion);
+          } else if (ctl == 'I') {
+            coeff_integral += 0.1;
+            Serial.print("coeff_integral = ");
+            Serial.println(coeff_integral);
+          } else if (ctl == 'i') {
+            coeff_integral -= 0.1;
+            Serial.print("coeff_integral = ");
+            Serial.println(coeff_integral);
+          } else if (ctl == 'D') {
+            coeff_derivative += 0.1;
+            Serial.print("coeff_derivative = ");
+            Serial.println(coeff_derivative);
+          } else if (ctl == 'd') {
+            coeff_derivative -= 0.1;
+            Serial.print("coeff_derivative = ");
+            Serial.println(coeff_derivative);
           }
         }
       }
@@ -26,6 +59,38 @@ void inject() {
       motor_write(0, 0);
       motor_write(1, 0);
       light_calibration_mode();
+    } else if (ctl == 'R') {
+      coeff_proportion += 0.1;
+      Serial.print("coeff_proportion = ");
+      Serial.println(coeff_proportion);
+    } else if (ctl == 'r') {
+      coeff_proportion -= 0.1;
+      Serial.print("coeff_proportion = ");
+      Serial.println(coeff_proportion);
+    } else if (ctl == 'I') {
+      coeff_integral += 0.1;
+      Serial.print("coeff_integral = ");
+      Serial.println(coeff_integral);
+    } else if (ctl == 'i') {
+      coeff_integral -= 0.1;
+      Serial.print("coeff_integral = ");
+      Serial.println(coeff_integral);
+    } else if (ctl == 'D') {
+      coeff_derivative += 0.1;
+      Serial.print("coeff_derivative = ");
+      Serial.println(coeff_derivative);
+    } else if (ctl == 'd') {
+      coeff_derivative -= 0.1;
+      Serial.print("coeff_derivative = ");
+      Serial.println(coeff_derivative);
+    } else if (ctl == 'P') {
+      deltaBase += 0.1;
+      Serial.print("deltaBase = ");
+      Serial.println(deltaBase);
+    } else if (ctl == 'p') {
+      deltaBase -= 0.1;
+      Serial.print("deltaBase = ");
+      Serial.println(deltaBase);
     }
   }
 }
