5a4fb928ecc0f82ace71974b2c90bffa486720c4
nyiyui <+@nyiyui.ca>
2024-01-16 10:23:28 -0500

    cleanup

--- culm.ino
+++ culm.ino
@@ -37,56 +37,15 @@ void setup() {
 void loop() {
   inject();
   light_read();
-  //halation();
   generic();
-  //steps();
   return;
 }
-
-template <typename T> int signum(T val) {
-  return (T(0) < val) - (val < T(0));
-}
-
-/*
-void halation() {
-  strip.setPixelColor(STATUS_MODE, 255, 0, 0);
-  static float prev;
-
-  float delta = get_delta()/(LIGHT_LEN/2);
-  Serial.print(" ");
-  Serial.print(delta);
-  Serial.print("d");
-  Serial.print(" ");
-  Serial.print(delta-prev);
-  Serial.print("deriv");
-  float derivative = delta-prev;
-  float dir = 0.5*delta;
-  float dd = abs(delta);// max(abs(delta), 10*abs(derivative));
-  float straight = -1.33*dd*dd+0.947;
-  motor_move2(dir, straight, 1.0);
-  prev = delta;
-  delay(10);
-  return;
-}
-*/
 
 void generic() {
   strip.setPixelColor(STATUS_MODE, 0, 0, 255);
   static float prev;
 
   float delta = get_delta()/(LIGHT_LEN/2);
-  /*
-  if (abs(delta) >= 1) {
-    motor_move2(0, -1.0, 1.0);
-    delay(10);
-  }
-  if (abs(delta) >= 1) {
-    steps();
-  }
-  */
-  //Serial.print(" ");
-  //Serial.print(delta);
-  //Serial.print("d");
   float derivative = delta-prev;
   float dir = 0.35*(0.93*delta*delta*delta+delta)+0.1*derivative;
   dir = constrain(dir, -1, 1);
@@ -96,34 +55,4 @@ void generic() {
   prev = delta;
   delay(40);
   return;
-}
-
-void steps() {
-  strip.setPixelColor(STATUS_MODE, 255, 255, 255);
-  strip.show();
-  float deltaInitial = light_normalized[2];
-  float history = abs(deltaInitial);
-  float prev = deltaInitial;
-  while (true) {
-    Serial.println(history);
-    {
-      float brightness = 255*abs(history);
-      strip.setPixelColor(STATUS_SUBMODE, brightness, brightness, brightness);
-      strip.show();
-    }
-    inject();
-    light_read();
-    float delta = get_delta()/(LIGHT_LEN/2);
-    float dir = 0.4*delta + 0.05*(delta-prev);
-    float straight = -0.167*abs(delta)*abs(delta) - 0.183*abs(delta) + 0.4;
-    straight *= 1.5;
-    straight = abs(straight);
-    motor_move2(dir, straight, 1.0);
-    history = 0.9*history + 0.1*(delta);
-    if (abs(history) < 0.2) {
-      motor_move2(0, 0, 0);
-      return;
-    }
-    prev = delta;
-  }
 }
